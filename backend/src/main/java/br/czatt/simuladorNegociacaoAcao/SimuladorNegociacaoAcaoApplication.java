c6ccddd3ee9a8b5f81cdef5aed8ff546c2999e59
CristianZatt <cristian.zatt@gmail.com>
2018-11-04 10:31:22 -0200

    Projeto base rodando.
    Criado projeto springboot backednd. Configurado conexão com banco.
    
    Criado projeto vue com o CLI3.
    Adicionado vuetify (tela).
    Adicionado axios (consulta de WS)

---
+++ backend/src/main/java/br/czatt/simuladorNegociacaoAcao/SimuladorNegociacaoAcaoApplication.java
@@ -0,0 +1,12 @@
+package br.czatt.simuladorNegociacaoAcao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladorNegociacaoAcaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladorNegociacaoAcaoApplication.class, args);
+	}
+}
