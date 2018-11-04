5814747aef9da0dc37f2bb036bef04f7c8dc182f
CristianZatt <cristian.zatt@gmail.com>
2018-11-04 12:01:01 -0200

    Interface de busca do cliente

---
+++ backend/src/main/java/br/czatt/simuladorNegociacaoAcao/repository/ClienteRepository.java
@@ -0,0 +1,21 @@
+package br.czatt.simuladorNegociacaoAcao.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.czatt.simuladorNegociacaoAcao.domain.Cliente;
+
+import java.util.List;
+
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+
+    List<Cliente> findByLogin(@Param("login") String login);
+
+    List<Cliente> findByNome(@Param("nome") String nome);
+    
+    
+    @Override
+    @Transactional(timeout = 10)
+    public List<Cliente> findAll();
+}
